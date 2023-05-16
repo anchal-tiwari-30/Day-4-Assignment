6c8e71ea0157a4a41f7d7c7f22e818e9c5901c8e
anchal-tiwari-30 <133504178+anchal-tiwari-30@users.noreply.github.com>
2023-05-17 00:58:50 +0530

    Update test.js

--- Que 6/test.js
+++ Que 6/test.js
@@ -1,17 +1,26 @@
 function checkGrade() {
-    var examType = document.getElementById('examType').value;
-    var totalMarksInput = document.getElementById('totalMarks');
-    var totalMarks = parseInt(totalMarksInput.value);
-    var resultElement = document.getElementById('result');
-  
-    var isAPlus = false;
-  
+  var examType = document.getElementById('examType').value;
+  var totalMarksInput = document.getElementById('totalMarks');
+  var totalMarks = parseInt(totalMarksInput.value);
+  var resultElement = document.getElementById('result');
+  var errorElement = document.getElementById('error');
+
+  if (totalMarks > 100) {
+    errorElement.textContent = "Error: Total marks cannot be greater than 100";
+    resultElement.textContent = "";
+    return;
+  }
+
+  var isAPlus = false;
+
+  if (totalMarks >= 0 && totalMarks <= 100) {
     if (examType === "Final-exam" && totalMarks >= 90) {
       isAPlus = true;
     } else if (examType === "Regular-exam" && totalMarks >= 89 && totalMarks <= 100) {
       isAPlus = true;
     }
-  
-    resultElement.textContent = "Grade: " + (isAPlus ? "A+" : "Not A+");
   }
-  
+
+  errorElement.textContent = "";
+  resultElement.textContent = "Grade: " + (isAPlus ? "A+" : "Not A+");
+}
